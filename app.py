7ec036ab313ea1ae5ff068b5c364e205e5a907fe
GuntKritamate <kritamate.agis@mail.kmutt.ac.th>
2021-11-09 15:38:57 +0700

    [FIX] Backend can connect with model

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from flask import Flask, render_template, url_for, jsonify, request
+import io
+import string
+import time
+import os
+import numpy as np
+import pickle
+import numpy as np
+# from sklearn.externals import joblib
+from model import predict,x_value
 
 app = Flask(__name__)
 
@@ -17,6 +26,8 @@ post = [
     },
 ]
 
+# joblib.dump(mymodel, "linear_regression_model.pkl")
+
 
 @app.route("/")
 
@@ -28,15 +39,27 @@ def home():
 def about():
     return render_template('about.html',title = 'About' )
  
-@app.route('/test', methods = ['GET','POST'])
-def test():
+@app.route('/prediction', methods = ['GET','POST'])
+def prediction():
     if request.method == 'GET':
-        return jsonify({'res':'200 Ok ><'})
+        return jsonify({'res':'200 Ok ><'},{'predict':predict},{'x_value':x_value})
     elif request.method == 'POST':
         req_Json = request.json
-        input = req_Json['name']
-        return jsonify({'res': 'Hi, ' + input})
+        input = req_Json['input_x']
+        return jsonify({'res': 'value_x, ' + input})
         
+# @app.route("/predict", methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
+#         try:
+#             data = request.get_json()
+#             years_of_experience = float(data["yearsOfExperience"])
+            
+#             lin_reg = joblib.load("./linear_regression_model.pkl")
+#         except ValueError:
+#             return jsonify("Please enter a number.")
+
+#         return jsonify(lin_reg.predict(years_of_experience).tolist())
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -2,13 +2,19 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import numpy
 from sklearn.metrics import r2_score
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pickle
+#import requests
+import json
 
-#x = [1,2,3,4,5,6,7]
-#y = [1,2,3,4,5,6,7]
+x = [1,2,3,4,5,6,7]
+y = [1,2,3,4,5,6,7]
 
-x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22] 
+#x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22] 
 #X as a time serie (in each day)
-y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+#y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
 #y as a selling number from input in our application
 
 for i in range(30):
@@ -19,19 +25,19 @@ for i in range(30):
   mymodel = numpy.poly1d(numpy.polyfit(x, y, i))
   pevScore = r2_score(y, mymodel(x))
   
-  print('round,',i)
+  #print('round,',i)
   if pevScore < nextScore:
     nextScore = pevScore
   elif nextScore <= pevScore:
     break
   
 
-print('r value ',r2_score(y, mymodel_i(x)))
-x_value = 21
+# print('r value ',r2_score(y, mymodel_i(x)))
+x_value = 9
 predict = mymodel(x_value)
-print('x value equal to ',x_value)
-print('Prediction: ',predict)
-plt.scatter(x, y)
+# print('x value equal to ',x_value)
+# print('Prediction: ',predict)
+# plt.scatter(x, y)
 myline = numpy.linspace(1, 22, 100)
-plt.plot(myline, mymodel(myline))
-plt.show()
+# plt.plot(myline, mymodel(myline))
+# plt.show()
